32c404be9ed2289a64059338a3cf026f1241016c
Maelton Lima <maelton.main@gmail.com>
2025-08-27 14:19:54 -0300

    feat(utils): add random password generator

--- src/main/java/br/com/brisabr/helpdesk/utils/hash/PasswordGenerator.java
+++ src/main/java/br/com/brisabr/helpdesk/utils/hash/PasswordGenerator.java
@@ -1,4 +1,13 @@
 package br.com.brisabr.helpdesk.utils.hash;
 
+import java.security.SecureRandom;
+import java.util.UUID;
+
 public class PasswordGenerator {
+    public static String generateRandomPassword() {
+        SecureRandom randomGenerator = new SecureRandom();
+            String uuidStr = UUID.randomUUID().toString().replace("-", "");
+            String randomLongAsHex = Long.toHexString(randomGenerator.nextLong());
+        return uuidStr + randomLongAsHex;
+    }
 }
