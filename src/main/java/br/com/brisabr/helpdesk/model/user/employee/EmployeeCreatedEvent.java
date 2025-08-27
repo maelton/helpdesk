497d88923da715882ccb891e7a657e4f092ff957
Maelton Lima <maelton.main@gmail.com>
2025-08-27 14:17:18 -0300

    feat(event): add employee creation events

--- src/main/java/br/com/brisabr/helpdesk/model/user/employee/EmployeeCreatedEvent.java
+++ src/main/java/br/com/brisabr/helpdesk/model/user/employee/EmployeeCreatedEvent.java
@@ -1,4 +1,11 @@
 package br.com.brisabr.helpdesk.model.user.employee;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@RequiredArgsConstructor
 public class EmployeeCreatedEvent {
+    private final Employee employee;
 }
--- src/main/java/br/com/brisabr/helpdesk/model/user/employee/EmployeeCreatedListener.java
+++ src/main/java/br/com/brisabr/helpdesk/model/user/employee/EmployeeCreatedListener.java
@@ -1,4 +1,31 @@
 package br.com.brisabr.helpdesk.model.user.employee;
 
+import br.com.brisabr.helpdesk.utils.mail.Email;
+import br.com.brisabr.helpdesk.utils.mail.EmailService;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
 public class EmployeeCreatedListener {
+
+    private final EmailService emailService;
+
+    public EmployeeCreatedListener(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleEmployeeCreated(EmployeeCreatedEvent event) {
+        Employee employee = event.getEmployee();
+        emailService.sendEmail(
+            new Email(
+                employee.getUsername(),
+                "User Created",
+                "Your password reset URL here"
+            )
+        );
+    }
 }
+
