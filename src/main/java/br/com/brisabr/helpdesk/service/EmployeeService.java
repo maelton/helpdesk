7890b9a060e8d82fd9aa4b530dbb1cf574fcaa4a
Maelton Lima <maelton.main@gmail.com>
2025-08-27 14:24:21 -0300

    feat(service): publish EmployeeCreated event on employee creation

--- src/main/java/br/com/brisabr/helpdesk/service/EmployeeService.java
+++ src/main/java/br/com/brisabr/helpdesk/service/EmployeeService.java
@@ -1,11 +1,16 @@
 package br.com.brisabr.helpdesk.service;
 
 import br.com.brisabr.helpdesk.model.user.employee.Employee;
+import br.com.brisabr.helpdesk.model.user.employee.EmployeeCreatedEvent;
 import br.com.brisabr.helpdesk.model.user.employee.dto.EmployeeCreateDTO;
 import br.com.brisabr.helpdesk.repository.EmployeeRepository;
+import br.com.brisabr.helpdesk.utils.hash.PasswordGenerator;
+import br.com.brisabr.helpdesk.utils.mail.EmailService;
 
 import jakarta.persistence.EntityNotFoundException;
 
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,11 +23,23 @@ import java.util.List;
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final EmailService emailService;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Transactional
     public Employee create(EmployeeCreateDTO dto) {
         Employee employee = toEntity(dto);
-        return employeeRepository.save(employee);
+        employee.setPassword(
+            passwordEncoder.encode(
+                PasswordGenerator.generateRandomPassword()
+            )
+        );
+        Employee created = employeeRepository.save(employee);
+        eventPublisher.publishEvent(
+            new EmployeeCreatedEvent(created)
+        );
+        return created;
     }
 
     @Transactional(readOnly = true)
