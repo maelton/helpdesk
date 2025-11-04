43ba57829814aecd9b9ee1f6edb4d780e5e27505
Maelton Lima <maelton.main@gmail.com>
2025-11-04 08:33:13 -0300

    feat(migrations): add default product categories seed

---
+++ src/main/resources/db/migration/V12__seed_default_product_category.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_product_category (id, name, description, created_at, updated_at)
+VALUES
+    (1, 'Software', 'Products related to software solutions', NOW(), NOW()),
+    (2, 'Hardware', 'Products related to hardware devices', NOW(), NOW()),
+    (3, 'Services', 'Products related to various services', NOW(), NOW());
